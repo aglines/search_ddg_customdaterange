b37e21029a4c0c12019bfd9a4fdd4dd0d23552ba
Andrew Glines <aglines@gmail.com>
2023-05-03 12:18:31 +0200

    changed to bkgd

--- ddg_daterange/background.js
+++ ddg_daterange/background.js
@@ -5,10 +5,11 @@ function addDateRange(requestDetails) {
     const dateString = '&df=y';
     // change this for other date ranges :
     // const dateString = '&df=m'; //month, &df=w for week
-    // Custom date syntax:
-    // const dateString = '&df=2010-01-01..2020-01-01'
-    // obviously having a custom date hard-coded here is weird but the point is zero user interaction,
-    // maybe incorporate that into the extension later
+
+    // Custom date syntax: 
+    //       const dateString = '&df=2010-01-01..2020-01-01'
+    // obviously a custom date being hard-coded is weird, but it means zero user interaction
+    // Later, maybe add option to pick custom dates a user constantly
 
     if (requestDetails.url.startsWith(ddgUrl) && !requestDetails.url.includes(dateString)) {
         const newUrl = requestDetails.url + dateString;
@@ -22,6 +23,14 @@ function addDateRange(requestDetails) {
 browser.webRequest.onBeforeRequest.addListener(
     addDateRange,
     { urls: ["https://duckduckgo.com/*"] },
+
     ["blocking"]
+    // "blocking" needed in order to redirect request
 
 );
+
+
+// notes
+// when addDateRange is called, why create const ddgUrl with the same URL? 
+// it already has the requestDetails.url object, so this isn't DRY
+// I could slice the asterisk off the end of the URL, but this is more readable
