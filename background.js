0fe065ae8ac95089529f7eb024ed31764fceb92a
Andrew Glines <aglines@gmail.com>
2023-05-04 14:01:40 +0200

    now current selection is shown

--- background.js
+++ background.js
@@ -1,25 +1,32 @@
 // Code wasn't storing browser data correctly, didn't realize Firefox can't do this,
 // instead will use browser runtime message to send data to background script
 
+// also current options are not displayed, lets fix this
+
 let currentOptions = {
   dateRangeOption: "n",
   fromDate: "",
   toDate: "",
 };
 
+// custom date queryString syntax:  &df=2023-04-10..2023-05-01
+
 function buildDateRangeQueryString(option, fromDate, toDate) {
+  let queryString = "";
+
   if (option === "custom" && fromDate && toDate) {
-    return `&df=${fromDate}&dto=${toDate}`;
+    queryString = `&df=${fromDate}..${toDate}`;
   } else if (option && ["n", "d", "w", "m", "y"].includes(option)) {
-    return `&df=${option}`;
+    queryString = `&df=${option}`;
   }
-  return "";
+ 
+  return queryString;
 }
+
 
 function addDateRange(requestDetails) {
   const ddgUrl = "https://duckduckgo.com/";
   const queryString = buildDateRangeQueryString(currentOptions.dateRangeOption, currentOptions.fromDate, currentOptions.toDate);
-
   if (requestDetails.url.startsWith(ddgUrl) && !requestDetails.url.includes(queryString)) {
     const newUrl = requestDetails.url + queryString;
     return {
@@ -34,6 +41,10 @@ browser.webRequest.onBeforeRequest.addListener(
   ["blocking"]
 );
 
-browser.runtime.onMessage.addListener((message) => {
-  currentOptions = message;
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "getOptions") {
+    sendResponse(currentOptions);
+  } else {
+    currentOptions = message;
+  }
 });
--- popup_options.js
+++ popup_options.js
@@ -1,4 +1,5 @@
 const dateRangeSelect = document.getElementById("dateRangeSelect");
+const currentSelection = document.getElementById("currentSelection"); 
 const customDateRange = document.getElementById("customDateRange");
 const fromDateInput = document.getElementById("fromDate");
 const toDateInput = document.getElementById("toDate");
@@ -25,5 +26,24 @@ function sendOptionsToBackground() {
     savedMessage.style.display = "none";
   }, 2000);
 }
+
+// display current options
+function updateOptionDisplays() {
+  dateRangeSelect.value = currentOptions.dateRangeOption;
+
+  if (currentOptions.fromDate) {
+    fromDateInput.value = currentOptions.fromDate;
+  }
+  if (currentOptions.toDate) {
+    toDateInput.value = currentOptions.toDate;
+  }
+
+  updateCustomDateRangeVisibility(); 
+}
+
+browser.runtime.sendMessage({ type: "getOptions" }).then(response => {
+  currentOptions = response;
+  updateOptionDisplays();
+});
 
 saveButton.addEventListener("click", sendOptionsToBackground);
