008548b9ed255e3e9404955283ca48570946459a
Andrew Glines <aglines@gmail.com>
2023-05-04 12:38:35 +0200

    add save button to js

--- popup_options.js
+++ popup_options.js
@@ -1,48 +1,29 @@
-const dateRangeSelect = document.getElementById('dateRangeSelect');
-const customDateRange = document.getElementById('customDateRange');
-const fromDateInput = document.getElementById('fromDate');
-const toDateInput = document.getElementById('toDate');
-
+const dateRangeSelect = document.getElementById("dateRangeSelect");
+const customDateRange = document.getElementById("customDateRange");
+const fromDateInput = document.getElementById("fromDate");
+const toDateInput = document.getElementById("toDate");
+const saveButton = document.getElementById("saveButton");
+const savedMessage = document.getElementById("savedMessage");
 
 function updateCustomDateRangeVisibility() {
-    if (dateRangeSelect.value === "custom") {
-        customDateRange.style.display = "block";
-    } else {
-        customDateRange.style.display = "none";
-    }
+  if (dateRangeSelect.value === "custom") {
+    customDateRange.style.display = "block";
+  } else {
+    customDateRange.style.display = "none";
+  }
 }
 
-dateRangeSelect.addEventListener('change', () => {
-    updateCustomDateRangeVisibility();
-    browser.storage.local.set({
-        dateRangeOption: dateRangeSelect.value,
-        fromDate: fromDateInput.value,
-        toDate: toDateInput.value,
-    });
-});
+function sendOptionsToBackground() {
+  browser.runtime.sendMessage({
+    dateRangeOption: dateRangeSelect.value,
+    fromDate: fromDateInput.value,
+    toDate: toDateInput.value,
+  });
 
-fromDateInput.addEventListener('change', () => {
-    browser.storage.local.set({ fromDate: fromDateInput.value });
-});
+  savedMessage.style.display = "inline";
+  setTimeout(() => {
+    savedMessage.style.display = "none";
+  }, 2000);
+}
 
-toDateInput.addEventListener('change', () => {
-    browser.storage.local.set({ toDate: toDateInput.value });
-});
-
-// Show popup with previously saved values
-browser.storage.local.get(["dateRangeOption", "fromDate", "toDate"]).then(result => {
-    if (result.dateRangeOption) {
-        dateRangeSelect.value = result.dateRangeOption;
-    }
-    if (result.fromDate) {
-        fromDateInput.value = result.fromDate;
-    }
-    if (result.toDate) {
-        toDateInput.value = result.toDate;
-    }
-    updateCustomDateRangeVisibility();
-});
-
-
-
-
+saveButton.addEventListener("click", sendOptionsToBackground);
